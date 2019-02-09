dab911e45413371b44e77a36aa7463f8bebc343e
Kristea Gamel <krgamel@gmail.com>
2019-02-09 14:05:10 -0600

    trying to figure out out to trigger fade-in animation in css on click
    Signed-off-by: Kristea Gamel <krgamel@gmail.com>

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,26 +1,41 @@
 class Carousel {
-    constructor(carousel){
+    constructor(carousel) {
+
         this.carousel = carousel;
         this.leftButton = this.carousel.querySelector('.left-button');
         this.rightButton = this.carousel.querySelector('.right-button');
-        this.images = this.carousel.querySelectorAll('img');
+        this.images = this.carousel.querySelectorAll('img')
 
         let carouselIndex = 0;
+        this.carouselIndex = carouselIndex;
+        this.images[carouselIndex].style.display = 'flex';
 
         this.leftButton.addEventListener('click', () => this.selectLeft(carouselIndex));
         this.rightButton.addEventListener('click', () => this.selectRight(carouselIndex));
     }
 
-    selectLeft(index) {
-            
-    }
 
-    selectRight () {
+     selectLeft(index) {
+         this.images[this.carouselIndex].style.display = 'none';
+        //  this.images[this.carouselIndex].style.opacity = '0';
+         if (this.carouselIndex <= 0) {this.carouselIndex = this.images.length}
+         this.carouselIndex--;
+         this.images[this.carouselIndex].style.display = 'flex';
+     }
+     selectRight(index) {
+         this.images[this.carouselIndex].style.display = 'none';
+        //  this.images[this.carouselIndex].style.opacity = '1';
+         if (this.carouselIndex >= this.images.length-1) {this.carouselIndex = -1}
+         this.carouselIndex++;
+         this.images[this.carouselIndex].style.display = 'flex';
+         this.images[this.carouselIndex].classList.add('fade-in');
+     }
 
-    }
+        
 }
 
-let carousel = document.querySelector('.carousel').forEach(carousel => new Carousel(carousel));
+let carousel = document.querySelector('.carousel')
+carousel = new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
